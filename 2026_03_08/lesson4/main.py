f316f07f4fc18d856fde204c226481a5e65cf37b
roberthsu2003 <roberthsu2003@gmail.com>
2026-03-09 14:30:32 +0800

    modify

--- 2026_03_08/lesson4/main.py
+++ 2026_03_08/lesson4/main.py
@@ -8,12 +8,17 @@ requirements: requests, pydantic
 
 from typing import Optional
 import requests
+import logging
 from pydantic import BaseModel, Field
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class Filter:
     def inlet(self, body: dict, __user__: dict | None = None) -> dict:
         user_message = body["messages"][-1]["content"]
-        print(f"[Filter Debug] 使用者輸入: {user_message}")  # 會出現在 Open WebUI 的 log
+        logger.info(f"[Filter] 使用者輸入: {user_message}")
+        logger.debug(f"[Filter] body keys:{body.keys()}")
         # ...
         return body
 
