188ddfcd2aae842d5306cee49511aa73248c093b
roberthsu2003 <roberthsu2003@gmail.com>
2026-03-12 13:00:39 +0800

    modify

---
+++ 2026_03_12/lesson1/src/examples/Example3State.tsx
@@ -0,0 +1,10 @@
+function Example3State(){
+    return(
+        <div className="example-card">
+            <h2>範例3:State(狀態)</h2>
+            <p>請完成範例3來實作此部分</p>
+        </div>
+    )
+}
+
+export default Example3State
---
+++ 2026_03_12/lesson1/src/examples/Example4UseEffect.tsx
@@ -0,0 +1,10 @@
+function Example4UseEffect(){
+    return(
+        <div className="example-card">
+            <h2>範例5:事件處理</h2>
+            <p>請完成範例5來實作此部分</p>
+        </div>
+    )
+}
+
+export default Example4UseEffect
